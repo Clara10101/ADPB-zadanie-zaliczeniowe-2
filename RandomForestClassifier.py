37e45b17f2b85ef59694cf1b56e6c6e4e9a9f8e7
Klara Trzcinska <klara.trzcinska@gmail.com>
2016-01-05 20:02:40 +0100

    Klasyfikacja na podstawie drzewa

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -4,11 +4,14 @@ import random
 inf = float('inf')
 
 class BinNode:
-    def __init__(self, values, left=None, right=None):
+
+    def __init__(self, values, left=None, right=None, condition=None, decision=None):
 
         self.left = left
         self.right = right
         self.values = values
+        self.condition = condition
+        self.decision = decision
 
     def is_leaf(self):
 
@@ -23,19 +26,40 @@ class BinNode:
         elif which == 'R':
             return self.right
 
+    def set_sons_values(self, n_features, X, y):
+
+        condition = self.find_best_division(n_features, X, y)
+
+        #jesli podzial jest mozliwy i potrzebny
+        if condition[0] != None and len(set(self.values_classes(y))) > 1:
+
+            self.condition = condition[0]
+            self.left = BinNode(condition[1])
+            self.right = BinNode(condition[2])
+
+            self.left.set_sons_values(n_features, X, y)
+            self.right.set_sons_values(n_features, X, y)
+
+        if len(set(self.values_classes(y))) == 1:
+            self.decision = list(set(self.values_classes(y)))[0]
+
+    def values_classes(self, y):
+        return y[self.values]
+
     def find_best_division(self, n_features, X, y):
         #wyznaczenie optymalnego podzialu w wezle
         #Dla kazdego wierzcholka bedziemy losowali n_features cech i tylko dla nich bedziemy sprawdzali wszystkie mozliwe wartosci
         #kryterium optymalnosci Gini impurity
-        #zwraca krotke postaci (kryterium podzialu, obserwacje w lewym synu, obserwacje w prawym synu)
+        #zwraca krotke postaci ((kolumna dla kryterium podzialu, kryterium podzialu, typ kolumny podzialu liczbowy/wyliczeniowy), obserwacje w lewym synu, obserwacje w prawym synu)
         #jesli nie ma mozliwosci ustanowienia takiego podzialu aby kazdy z synow zawieral jakies wartosci krotka postaci (None, None, None)
 
         m, n = X.shape
         data_type, classifier_classes = analyse_input_data(X, y)
         random_features = random.sample(range(1, n), n_features)
-        best_division_condition = None; best_L_values = None; best_R_values = None
+
+        best_division_condition = None; best_L_values = None; best_R_values = None;
         best_gini_impurity = inf
-        n_all = self.values
+        n_all = len(self.values)
 
         #sprawdzenie wszystkich mozliwych wartosci dla kazdej z wylosowanych cech
         for i in random_features:
@@ -49,7 +73,7 @@ class BinNode:
                             L_values.append(value)
                         else:
                             R_values.append(value)
-                    else:
+                    else: #wyliczeniowe
                         if X[value,i] == j:
                             L_values.append(value)
                         else:
@@ -68,38 +92,71 @@ class BinNode:
                     #sprawdzenie czy do tej pory najlepszy warunek - minimalizuje gini
                     if gini_impurity < best_gini_impurity:
                         best_gini_impurity = gini_impurity
-                        best_division_condition = j
+                        best_division_condition = (i,j,data_type[i][0])
                         best_L_values = L_values
                         best_R_values = R_values
 
         return best_division_condition, best_L_values, best_R_values
 
+    def classify(self,v):
+        #klasyfikuje wektor v na podstawie drzewa zawieszonego w wierzcholku self
 
+        if not self.is_leaf():
+            if self.condition[2] == 'numeryczne':
+                if float(v[self.condition[0]]) <= float(self.condition[1]):
+                    return self.son("L").classify(v)
+                else:
+                    return self.son("R").classify(v)
+            else: #wyliczeniowe
+                if v[self.condition[0]] == self.condition[1]:
+                    return self.son("L").classify(v)
+                else:
+                    return self.son("R").classify(v)
+        else:
+            return self.decision
 
     def __repr__(self):
-        return 'Tree(%r, %r, %r)' % (self.values, self.left, self.right)
+
+        if self.is_leaf():
+            return 'Leaf(%r, %r)' % (self.values, self.decision)
+        else:
+            return 'Node(%r, %r)' % (self.values, self.condition)
 
 
 class BinTree:
-    def __init__(self, node):
-        self.node = node
+
+    def __init__(self, n_features, X, y):
+
+        m, n = X.shape
+        self.n_features = n_features
+        self.X = X
+        self.y = y
+
+        self.node = BinNode([i for i in range(m)])
+        self.node.set_sons_values(n_features, X, y)
 
     def root(self):
         return self.node
 
+    def classify(self,v):
+        return self.root().classify(v)
+
+
 
 class RandomForestClassifier:
+
     def __init__(self, n_features):
-        self.n = n_features
+        self.n_features = n_features
+        self.forest = []
 
     def fit(self, X, y):
         pass
 
     def create_decision_tree(self, X, y):
-        m, n = X.shape
 
-        tree = BinTree([i for i in range(m)])
-        # znalezienie najlepszego warunku podzialu
+        tree = BinTree(self.n_features, X, y)
+        return tree
+
 
 
 def analyse_input_data(X, y):
@@ -147,6 +204,16 @@ def gini(n, nl, nr, nl0, nl1, nr0, nr1):
     return (nl / n) * (nl0 / nl * (1 - nl0 / nl) + nl1 / nl * (1 - nl1 / nl)) + (nr / n) * (
     nr0 / nr * (1 - nr0 / nr) + nr1 / nr * (1 - nr1 / nr))
 
+def showR(node, prefix=''):
+    if node.is_leaf():
+        return prefix + '-' + str(node) + '\n'
+    else:
+        return showR(node.son('L'),prefix+'   ') + prefix + '-<' + '\n' + showR(node.son('R'),prefix+'   ')
+
+def show(tree):
+    return showR(tree.root())
+
+
 
 # t=Tree(5,Tree(6,7),7)
 #print t
@@ -156,5 +223,13 @@ dane_test_y = np.array(['KUP', 'NIE_KUPUJ', 'NIE_KUPUJ','KUP', 'NIE_KUPUJ'])
 #analyse_input_data(dane_test_X, dane_test_y)
 #przykladowy wynik wywolania
 #[('wyliczeniowe', ['Honda']), ('numeryczne', ['2009', '2006', '2005']), ('wyliczeniowe', ['igla', 'idealny']), ('numeryczne', ['180000.87', '10100', '215000'])]
-wierzchilek1 = BinNode([0,1,2,3,4])
-print wierzchilek1.find_best_division(3,dane_test_X, dane_test_y)
+#wierzchilek1 = BinNode([0,1,2,3,4])
+#print wierzchilek1.find_best_division(3,dane_test_X, dane_test_y)
+
+r = RandomForestClassifier(3)
+tree = r.create_decision_tree(dane_test_X, dane_test_y)
+print tree.root()
+#print show(tree)
+
+przyklad_testowy = ['Renault', 2005, 'bezkolizyjny', 215000]
+print tree.classify(przyklad_testowy)
