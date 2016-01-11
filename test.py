ee6ddf29add77c85cf4de7e61ab6638844a9bd91
mtarka111 <mtarka.111@gmail.com>
2016-01-11 15:44:45 +0100

    4mery

--- test.py
+++ test.py
@@ -22,31 +22,38 @@ def generate_kmers(k): #generuje wszystkie k-mery zlozone z liter A, C, T, G dlu
         kmers.append(kmer)
     return kmers
 
+def delete_not_occuring_kmer(arr):
+    for i in range(len(arr[0])):
+        for j in arr:
+            tmp=False
+            if j[i]>0:
+                tmp=True
+                break
+        if tmp==False:
+            arr=np.delete(arr,i,0)
+    return arr
+
+
 def test():
     enhancers=read_file('enhancers_heart.fa')
     random=read_file('random.fa')
-    enh=open('enh.txt','w')
-    ran=open('ran.txt','w')
-    kmer_file=open('kmers.txt','w')
-    km=[]
-    e,r=[],[]
-    for i in range(1,10):
-        kmers=generate_kmers(i)
-        km=km+kmers
+    kmers_in_seq=[]
+    kmers=generate_kmers(4)
+    y=[]
+    for j in enhancers:
+        k_n=[]
         for k in kmers:
-            number_e=0
-            number_r=0
-            for j in enhancers:
-                number_e=number_e+j.count(k)
-            for j in random:
-                number_r=number_r+j.count(k)
-            e.append(number_e)
-            r.append(number_r)
-    pickle.dump(np.array(e),enh)
-    enh.close()
-    pickle.dump(np.array(r),ran)
-    ran.close()
-    pickle.dump(np.array(km),kmer_file)
-    kmer_file.close()
+            k_n.append(j.count(k))
+        kmers_in_seq.append(k_n)
+        y.append('enhancer')
+    for j in random:
+        k_n=[]
+        for k in kmers:
+            k_n.append(j.count(k))
+        kmers_in_seq.append(k_n)
+        y.append('random')
+    kmers_in_seq=delete_not_occuring_kmer(np.array(kmers_in_seq))
+    return kmers_in_seq, y
 
-test()
+
+kmer,y=test()
